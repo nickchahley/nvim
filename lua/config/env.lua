a127512634ee5564e321e05458a34856de034bfb
nickchahley <nickchahley@gmail.com>
2023-08-15 10:30:50 -0400

    Revert changes b/c expd slowdown

--- lua/config/env.lua
+++ lua/config/env.lua
@@ -1,6 +1,5 @@
 -- just assume nix not windows
-vim.env.VIMHOME      = vim.env.HOME .. '/.config/nvim'
-vim.env.VH           = vim.env.VIMHOME -- alias
-vim.env.VIMSHARE     = vim.env.HOME ..'/.local/share/nvim'
+vim.env.VIMHOME = vim.env.HOME .. '/.config/nvim'
+vim.env.VIMSHARE = vim.env.HOME ..'/.local/share/nvim'
 vim.env.BACKUNDOSWAP = vim.env.HOME .. '/.vim-backundoswap/'
-vim.env.FUNCTIONS    = vim.env.VIMHOME .. '/functions'
+vim.env.VH = vim.env.VIMHOME
