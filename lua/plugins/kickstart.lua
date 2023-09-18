9b1dd71ac159d32de53ac607bb18daa0daa1cc47
nickchahley <nickchahley@gmail.com>
2023-09-18 09:13:40 -0400

    Move configs out of lazy.lua

--- lua/plugins/kickstart.lua
+++ lua/plugins/kickstart.lua
@@ -53,6 +53,17 @@ local M = {
 			-- Additional lua configuration, makes nvim stuff amazing!
 			{ 'folke/neodev.nvim', opts = {} },
 		},
+		config = function()
+			require 'plugins.config.lspconfig'
+
+			-- config of diagnostic and virtual text
+			-- what I am after:
+			--  use symbols in the gutter, maybe underlines, to identify diagnostic flags
+			--  maybe be able to toggle virtual text
+			--  I would like it if the gutter just stayed expanded the whole time so that my
+			vim.diagnostic.config({ virtual_text = false })
+			vim.opt.signcolumn = 'yes'
+		end,
 	},
 	-- Autocompletion
 	--[[ {
@@ -110,16 +121,113 @@ local M = {
 					},
 				},
 			},
-		}
+		},
+		config = function()
+			-- Enable telescope fzf native, if installed
+			pcall(require('telescope').load_extension, 'fzf')
+
+			-- See `:help telescope.builtin`
+			vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+			vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+			vim.keymap.set('n', '<leader>/', function()
+				-- You can pass additional configuration to telescope to change theme, layout, etc.
+				require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+					winblend = 10,
+					previewer = false,
+				})
+			end, { desc = '[/] Fuzzily search in current buffer' })
+
+			vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+			vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+			vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+			vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch cburrent [W]ord' })
+			vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+			vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+			vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]resume' })
+			vim.keymap.set('n', '<leader>sb', require('telescope.builtin').current_buffer_fuzzy_find, { desc = '[S]earch [b]uffer' })
+		end,
 	},
 
+	-- Highlight, edit, and navigate code
 	{
-		-- Highlight, edit, and navigate code
 		'nvim-treesitter/nvim-treesitter',
 		dependencies = {
 			'nvim-treesitter/nvim-treesitter-textobjects',
 		},
 		build = ':TSUpdate',
+		config = function()
+			require('nvim-treesitter.configs').setup {
+				-- Add languages to be installed here that you want installed for treesitter
+				ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'r' },
+
+				-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+				auto_install = false,
+
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = '<c-space>',
+						node_incremental = '<c-space>',
+						scope_incremental = '<c-s>',
+						node_decremental = '<M-space>',
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							['aa'] = '@parameter.outer',
+							-- ['ia'] = '@parameter.inner',
+							['af'] = '@function.outer',
+							-- ['if'] = '@function.inner',
+							['ac'] = '@class.outer',
+							-- ['ic'] = '@class.inner',
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							[']m'] = '@function.outer',
+							[']]'] = '@class.outer',
+						},
+						goto_next_end = {
+							[']M'] = '@function.outer',
+							[']['] = '@class.outer',
+						},
+						goto_previous_start = {
+							['[m'] = '@function.outer',
+							['[['] = '@class.outer',
+						},
+						goto_previous_end = {
+							['[M'] = '@function.outer',
+							['[]'] = '@class.outer',
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							['<leader>a'] = '@parameter.inner',
+						},
+						swap_previous = {
+							['<leader>A'] = '@parameter.inner',
+						},
+					},
+				},
+			}
+
+			-- Diagnostic keymaps
+			vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+			vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+			vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+			vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+
+		end,
 	},
 }
 
