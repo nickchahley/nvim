31b1b484ea104c3ae7e9bb7a71b54ef05770767d
nickchahley <nickchahley@gmail.com>
2023-10-04 20:40:48 -0400

    Add listeners for dapui

--- lua/plugins/config/dap.lua
+++ lua/plugins/config/dap.lua
@@ -1,4 +1,8 @@
--- dap keybindings, global so they can start the debugger
+-- local opts = { noremap = true, silent = true }
+-- local keymap = vim.api.nvim_set_keymap
+-- keymap('n', <KEYS>, <COMMAND>, opts)
+-- dap keybindings
+-- at least continue and set_breakpoint need to be global so they can start the debugger
 vim.keymap.set('n', '<F5>', ":lua require'dap'.continue()<CR>")
 vim.keymap.set('n', '<leader>dc', ":lua require'dap'.continue()<CR>")
 vim.keymap.set('n', '<F6>', ":lua require'dap'.restart()<CR>")
@@ -12,18 +16,6 @@ vim.keymap.set('n', '<leader>lp', ":lua require'dap'.set_breakpoint(nil, nil, vi
 vim.keymap.set('n', '<leader>dr', ":lua require'dap'.repl.open()<CR>")
 vim.keymap.set('n', '<leader>dui', ":lua require'dapui'.toggle()<CR>")
 
-
--- dap-python
--- register the "adapter and configurations"
-require('dap-python').setup('~/.virtualenvs/debugpy/bin/python', {
-	console = 'integratedTerminal',
-})
-
--- force python path "for now"
-require('dap-python').resolve_python = function()
-  return '/usr/bin/python3'
-end
-
 require('dapui').setup({
 	{ -- user_config
 		mappings = {
@@ -35,11 +27,36 @@ require('dapui').setup({
 		},
 	}
 })
--- local opts = { noremap = true, silent = true }
--- local keymap = vim.api.nvim_set_keymap
--- keymap('n', <KEYS>, <COMMAND>, opts)
+
+local dap, dapui = require('dap'), require('dapui')
+dap.listeners.after.event_initialized['dapui_config'] = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated['dapui_config'] = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited['dapui_config'] = function()
+	dapui.close()
+end
+
+-- dap-python
+-- register the "adapter and configurations"
+local dap_py = require('dap-python')
+dap_py.setup('~/.virtualenvs/debugpy/bin/python', {
+	console = 'integratedTerminal',
+})
+
+-- force python path "for now"
+dap_py.resolve_python = function()
+  return '/usr/bin/python3'
+end
+
 
 -- enable type checking for nvim-dap-ui to get type checking, documentation and autocompletion for all API functions.
 require("neodev").setup({
   library = { plugins = { "nvim-dap-ui" }, types = true },
 })
+
+
+-- FUTURE
+-- require("nvim-dap-virtual-text").setup {commented = true,}
