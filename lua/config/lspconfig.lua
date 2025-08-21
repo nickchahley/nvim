4a88d28ec9fd36ee2f6d417d9d03d562851703a5
nickchahley <nickchahley@gmail.com>
2025-08-21 14:27:09 -0400

    update mason-lspconfig to 2.0

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -54,36 +54,40 @@ local servers = {
     filetypes = { "r", "rmd", "Rmd" },
     log_level = 2
   },
-  -- bashls = {},
   vimls = {},
   cssmodules_ls = {},
   lua_ls = {
-    Lua = {
+    settings = {
+      Lua = {
+        runtime = {
+          version = 'LuaJIT',
+        },
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
-      diagnostics = { globals = {'vim'} },
+        diagnostics = {
+          globals = {
+            'vim',
+            'require',
+          },
+        },
+      },
     },
-  },
+  }
 }
+
+for k, v in pairs(servers) do
+  vim.lsp.config(k, v)
+end
+
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 -- local capabilities = vim.lsp.protocol.make_client_capabilities()
 -- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Ensure the servers above are installed
+require('mason').setup()
 local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end
 }
