54aa9d368718bb7cff5fa492bb81b2995b83d517
nickchahley <nickchahley@gmail.com>
2026-06-02 16:48:23 -0400

    adjust lspconfig

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,29 +1,34 @@
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-  local nmap = function(keys, func, desc)
+  local map = function(keys, func, desc, mode)
+    mode = mode or 'n'
     if desc then
       desc = 'LSP: ' .. desc
     end
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
   end
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-  nmap('<leader>k', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<leader>K', vim.lsp.buf.signature_help, 'Signature Documentation')
+  map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+  map('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+  map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  map('<leader>k', vim.lsp.buf.hover, 'Hover Documentation')
+  map('<leader>K', vim.lsp.buf.signature_help, 'Signature Documentation')
 
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
+
+  -- Execute a code action, usually your cursor needs to be on top of an error
+  -- or a suggestion from your LSP for this to activate.
+  map('ga', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+
+  map('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  map('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  map('<leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
 
@@ -31,6 +36,14 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+
+
+  -- create a keymap to toggle inlay hints in your code, if the language server you are using supports them
+  --
+  -- This may be unwanted, since they displace some of your code
+  if client and client:supports_method('textDocument/inlayHint', event.buf) then
+    map('<leader>th', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }) end, '[T]oggle Inlay [H]ints')
+  end
 
 end
 
@@ -91,6 +104,7 @@ for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
   vim.lsp.config(server, {
       on_attach = on_attach,
   })
+  vim.lsp.enable(server)
 end
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
--- lua/modules/dev.lua
+++ lua/modules/dev.lua
@@ -28,8 +28,10 @@ local M = {
   { 'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      {'williamboman/mason-lspconfig.nvim'},
+      -- { 'williamboman/mason.nvim', config = true },
+      -- {'williamboman/mason-lspconfig.nvim'},
+      { 'mason-org/mason.nvim', config = true },
+      {'mason-org/mason-lspconfig.nvim'},
 
       -- Useful status updates for LSP
       { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
